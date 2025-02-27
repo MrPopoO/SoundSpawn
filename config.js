643f736216343e6034d3cf0076b0b749627cbf12
MrPopoO <131968233+MrPopoO@users.noreply.github.com>
2025-02-27 20:35:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://MrPopo00:MyStrong@21#1998&PassWo@discordbot.csd0y.mongodb.net/?retryWrites=true&w=majority",
+  mongodbUri : "mongodb+srv://mrpopo00:sonu211998@discordbot.qy7n7.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
