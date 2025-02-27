72681fa7da0b23ec7e70fb8009c4267f1e152010
MrPopoO <131968233+MrPopoO@users.noreply.github.com>
2025-02-27 18:09:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://MrPopo00:MyStrong@21#1998&PassWo@discordbot.csd0y.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
